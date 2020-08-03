fd8e0d35c067270874cd0b147a47fd70bd332187
tinas <tinas@Plato>
2020-08-03 15:28:53 +0200

    Comparable implements

---
+++ Comparator_Auto/src/Auto.java
@@ -0,0 +1,132 @@
+public class Auto extends Fahrzeug 
+{
+    private boolean automatik;
+
+    public Auto(String kennzeichen, int baujahr, boolean automatik) throws AutoException
+    {
+    	super(kennzeichen, baujahr);
+        setAutomatik(automatik);
+    }
+
+    public Auto(String kennzeichen) throws AutoException
+    {
+    	super(kennzeichen, 2007);
+        setAutomatik(false);
+    }
+
+    public Auto(String kennzeichen, boolean automatik) throws AutoException
+    {
+    	super(kennzeichen, 2007);
+        setAutomatik(automatik);
+    }
+
+    public Auto() throws AutoException
+    {
+    	super();
+        setAutomatik(false);        
+    }
+
+    
+    public boolean getAutomatik()
+    {
+        return automatik;
+    }
+    
+    public void setAutomatik(boolean automatik)
+    {
+        this.automatik = automatik;
+    }
+    
+    public String toString()
+    {
+        if (automatik == true)
+        {
+            return super.toString() + " (AUTOMATIK)";
+        }
+        else
+        {
+            return super.toString() + " (kein AUTOMATIK)";
+        }
+    }
+    public void setBaujahr(int baujahr) throws AutoException
+    {
+        if (automatik == true)
+        {
+            //1985..2017
+            if ((baujahr>=1985) && (baujahr<=2017))
+            {
+                super.setBaujahr(baujahr);
+            }
+            else
+            {
+                if (baujahr<1985)
+                {
+                    //zu klein
+                	throw new AutoException("Fehler: Automatik Autos gibt erst ab dem Baujahr 1985!");
+                }
+                else
+                {
+                    //zu groß
+                	throw new AutoException("Fehler: Automatik Autos gibt bis zum Baujahr 2017!");
+                }
+            }
+        }
+        else
+        {
+            //1960..2007
+            if ((baujahr>=1960) && (baujahr<=2007))
+            {
+                super.setBaujahr(baujahr);
+            }
+            else
+            {
+                if (baujahr<1960)
+                {
+                    //zu klein
+                	throw new AutoException("Fehler: Autos mit Schaltung gibt es ab dem Baujahr 1960!");
+                }
+                else
+                {
+                    //zu groß
+                	throw new AutoException("Fehler: Autos mit Schaltung gibt bis zum Baujahr 2007!");
+                }
+            }
+        }
+    }
+
+    
+    public void ausparken() throws AutoException
+    {
+        if (getParkplatz() != null)
+        {
+            super.getParkplatz().ausparken(this);
+        }
+        else
+        {
+            throw new AutoException("Fehler: stehe auf keinem Parkplatz!");
+        }
+    }
+    
+    public double getGeld()
+    {
+    	if (automatik == true)
+    	{
+    		return 12;
+    	}
+    	else
+    	{
+    		return 10;
+    	}
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Comparator_Auto/src/Parkplatz.java
@@ -0,0 +1,191 @@
+import java.util.*;
+
+public class Parkplatz
+{
+    private ArrayList<Fahrzeug> platze;
+
+    public Parkplatz()
+    {
+        Auto a;
+
+        platze = new ArrayList<Fahrzeug>();
+    }
+    
+    public void sortBaujahr()
+    {
+    	Collections.sort(platze, new Fahrzeug.BaujahrComparator());
+    }
+    
+    public void sortKennzeichen()
+    {
+    	Collections.sort(platze, new Fahrzeug.KennzeichenComparator());
+    }
+    
+    public void sortAutoId()
+    {
+    	Collections.sort(platze);
+    }
+
+    public void ausparken(String ort) throws AutoException
+    {
+        Iterator<Fahrzeug> it;
+        Fahrzeug a;
+        
+        if (ort != null)
+        {
+            it = platze.iterator();
+            while (it.hasNext())
+            {
+                a = it.next();
+                if (a.getKennzeichen().startsWith(ort))
+                {
+                    it.remove();
+                    a.setParkplatz(null);
+                }
+            }
+        }
+        else
+        {
+            throw new AutoException("Fehler: kein Ort!");
+        }
+    }
+    
+    public void einparken(Fahrzeug neu) throws AutoException
+    {
+        if (neu != null)
+        {
+            if (platze.size() < 5)
+            {
+            	if (neu.getParkplatz() == null)
+            	{
+            		platze.add(neu);
+            		neu.setParkplatz(this);
+            	}
+            	else
+            	{
+            		throw new AutoException("FEHLER: schon auf einem anderen Parkplatz!");
+            	}
+            }
+            else
+            {
+            	throw new AutoException("Fehler: kein Platz mehr!");
+            }
+        }
+        else
+        {
+        	throw new AutoException("Fehler: kein Auto!");
+        }
+    }
+
+    public String toString()
+    {
+        String erg;
+
+        erg = "";
+        for (Fahrzeug a: platze)
+        {
+            erg = erg + a.toString() + "\n";
+        }
+        return erg;
+    }
+
+    public int anzahlAutomatik()
+    {
+        int anz;
+
+        anz = 0;
+        for (Fahrzeug f: platze)
+        {
+            if (f instanceof Auto)
+            {
+            	Auto a = (Auto)f;
+            	if (a.getAutomatik())
+            	{
+            		anz++;
+            	}
+            }
+        }
+        return anz;
+    }
+
+    public double avgBaujahrAutomatik() throws AutoException
+    {
+        int sum;
+        int anz;
+
+        sum=0;
+        anz=0;
+        for (Fahrzeug f: platze)
+        {
+        	if (f instanceof Auto)
+        	{
+        		Auto a = (Auto)f;
+        		if (a.getAutomatik() == true)
+        		{
+        			sum += a.getBaujahr();
+        			anz++;
+        		}
+        	}
+        }
+        if (anz > 0)
+        {
+            return (double)sum/anz;
+        }
+        else
+        {
+        	throw new AutoException("Fehler: kein automatik Auto!");
+        }
+    }
+
+    public int minBaujahr()
+    {
+        int min;
+
+        min = 9999;
+        for (Fahrzeug a: platze)
+        {
+            if (a.getBaujahr() < min)
+            {
+                min = a.getBaujahr();
+            }
+        }
+        return min;
+    }
+
+    public void ausparken(Fahrzeug wer) throws AutoException
+    {
+        if (wer != null)
+        {
+            if (platze.remove(wer)) /* == true */
+            {
+                wer.setParkplatz(null);
+            }
+            else
+            {
+            	throw new AutoException("Fehler: Auto nicht am Parkplatz!");
+            }
+        }
+        else
+        {
+        	throw new AutoException("Fehler: kein Auto!");
+        }
+    }
+    
+    public double abkassieren()
+    {
+    	double summe;
+    	
+    	summe=0;
+    	for (Fahrzeug f: platze)
+    	{
+    		summe += f.getGeld();
+    	}
+    	return summe;
+    }
+}
+
+
+
+
+
+
